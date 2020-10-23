1b6b783ab077a8095fb6da0f8102f20e5d578292
JohanRestrepo <esnic1994@gmail.com>
2020-10-22 22:17:02 -0500

    apenas he configurado la pagina inicial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Consola extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date(),
+      name:User1.name,
+    };
+    this.accion1 = this.accion1.bind(this);
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  accion1(dato) {
+    if(dato == 'retirar'){
+      window.alert('desea retirar');
+    }
+    else if(dato == 'ingresar'){
+      window.alert('desea ingresar');
+    }
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    const Name = <Welcome name={this.state.name} />;
+    return (
+    <div>
+      <center>
+      {Name}
+      <h1>Bienvenido a banca johan!</h1>
+      <h2>La hora es {this.state.date.toLocaleTimeString()}.</h2>
+      <p>¿En que podemos ayudarte el día de hoy?</p>
+      <table>
+        <tr>
+          <td>
+          <button class="btn btn-primary" onClick={this.accion1.bind(this, 'retirar')}>
+          Retirar dinero
+          </button>
+          </td>
+          <td>
+          <button class="btn btn-primary" onClick={this.accion1.bind(this, 'ingresar')}>
+          Ingresar dinero
+          </button>
+          </td>
+        </tr>
+      </table>
+      </center>
+    </div>
+  );  
+  }
+}
+
+
+
+const User1 = {
+  name: 'Alveiro',
+  lastName: 're Alveiro',
+  password: 'kevin-gay',
+  Ahorro: 10000,
+};
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+ReactDOM.render(<Consola/>,
+ document.getElementById('root')
+ );
